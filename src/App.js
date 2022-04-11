87966f2d26ae6ca8b7c7cfd382b955e350038903
Sarfraz <alisarfraz900@gmail.com>
2022-04-11 20:14:51 +0530

    Alerts Added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar'
 import TextForm from './Components/TextForm';
+import { useState } from 'react';
+import Alert from './Components/Alert';
+import { isElementOfType } from 'react-dom/test-utils';
 function App() {
-  return (
+
+  const [mode,setmode]=useState('light')
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.background='black'
+      showAlert('Dark Mode Enabled','success')
+    } else {
+      setmode("light");
+      document.body.style.background='white'
+      showAlert('Light Mode Enabled','success')
+    }
+  };
+
+  const [alert,setalert]=useState(null)
+
+  const showAlert=(message,type)=>
+  {
+    setalert({
+      msg:message,
+      type:type
+    })
+
+    setTimeout(() => {
+      setalert(null)
+      
+    }, 2000);
+  }
+
+return (
     <>
-    <Navbar title='TextUtils'  about='AboutTextUtils'/>
-    <div className="container"><TextForm heading='Your Text Here To Check'/></div>
+    <Navbar title='TextUtils'  about='AboutTextUtils' toggle={togglemode} navmode={mode} />
+    <Alert alert={alert} />
+    <div className="container"><TextForm showAlert={showAlert}  toggle={togglemode} heading='Enter Text To analize'/></div>
     
     </>
   
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,88 +1,105 @@
 import React, { useState } from "react";
 
+
 export default function TextForm(props) {
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
 
-    const [myStyle,setMyStyle]=useState({
-        color:'black',
-        backgroundColor:'white'
-    })
+  const [theme, setTheme] = useState("Enable Dark Mode");
 
-    const [theme,setTheme]=useState('Enable Dark Mode')
-
-
-    const toggleStyle=()=>{
-        if(myStyle.color==='black')
-        {
-            setMyStyle({
-                color:'white',
-                backgroundColor:'black'
-            })
-            setTheme('Enable Light Mode')
-            // console.log('light now')
-        }
-        else{
-            setMyStyle({
-                color:'black',
-                backgroundColor:'white'
-            })
-            setTheme('Enable Dark Mode')
-            // console.log('dark now')
-        }
-
+  const toggleStyle = () => {
+    if (myStyle.color === "black") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+      });
+      setTheme("Enable Light Mode");
+      // console.log('light now')
+      props.toggle()
+      
+      
+    } else {
+      
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      setTheme("Enable Dark Mode");
+      // console.log('dark now')
+      props.toggle()
     }
- const upperCase = () => {
-    // console.log("upper case button clicked");
-    setText(text.toUpperCase())
+    
+  };
 
+  const upperCase = () => {
+    // console.log("upper case button clicked");
+    setText(text.toUpperCase());
+    props.showAlert("Changed To UPPERCASE",'success')
   };
 
   const lowerCase = () => {
     // console.log("lower case button clicked");
-    setText(text.toLowerCase())
-
+    setText(text.toLowerCase());
+    props.showAlert("Changed To lowercase",'success')
   };
 
   const handleOnChange = (event) => {
     // console.log("On change fired");
-    setText(event.target.value)
+    setText(event.target.value);
   };
 
   const [text, setText] = useState("Your Text Here");
   //  setText('i changed the text using settext')
 
   return (
-      <>
-    <div >
-      <div className="mb-3 my-3"  >
-        <h1>{props.heading}</h1>
-        <textarea
-          className="form-control "
-          style={myStyle}
-          onChange={handleOnChange}
-          value={text}
-          id="exampleFormControlTextarea1"
-          rows="10"
-        ></textarea>
-        <button className="btn btn-primary my-3" style={myStyle}  onClick={upperCase}>
-          Convert to UPPERCASE
-        </button>
-        <button className="btn btn-primary my-3 mx-3" style={myStyle} onClick={lowerCase}>
-          Convert to lowercase
-        </button>
-        <button className="btn btn-primary my-3 mx-3" style={myStyle} onClick={toggleStyle}>
-          {theme}
-        </button>
+    <>
+      <div>
+        <div className="mb-3 my-3">
+          <h1 style={myStyle} >{props.heading}</h1>
+          <textarea
+            className="form-control "
+            style={myStyle}
+            onChange={handleOnChange}
+            value={text}
+            id="exampleFormControlTextarea1"
+            rows="10"
+          ></textarea>
+          <button
+            className="btn btn-primary my-3"
+            style={myStyle}
+            onClick={upperCase}
+          >
+            Convert to UPPERCASE
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-3"
+            style={myStyle}
+            onClick={lowerCase}
+          >
+            Convert to lowercase
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-3"
+            style={myStyle}
+            onClick={toggleStyle}
+          >
+            {theme}
+          </button>
+        </div>
       </div>
-    </div>
 
-    <div style={myStyle} className="container">
+      <div style={myStyle} className="container">
         <h1>Your Text Summary</h1>
-        <p>Words-{text.split(' ').length} Characters-{text.length}</p>
-        <p>{(0.008*text.split(' ').length).toFixed(2)} Minutes to read</p>
+        <p>
+          Words-{text.split(" ").length} Characters-{text.length}
+        </p>
+        <p>{(0.008 * text.split(" ").length).toFixed(2)} Minutes to read</p>
         <h2>Preview</h2>
         <p>{text}</p>
         <p></p>
-    </div>
- </>
+      </div>
+    </>
   );
 }
